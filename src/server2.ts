bdaa4f4a5dedb490d62fafb3328fc949266056fc
idaxyt <waitingida.xyt@gmail.com>
2019-06-08 19:14:55 +0800

    server2添加Access-Control-Allow-Origin允许的网站，解决跨域问题

--- src/server2.ts
+++ src/server2.ts
@@ -2,9 +2,9 @@ import * as http from 'http'
 
 http.createServer(function(request,response) {
     console.log('request 9001',request.url)
-    // response.writeHead(200,{
-    //     "Access-Control-Allow-Origin": "http://localhost:9000"
-    // })
+    response.writeHead(200,{
+        "Access-Control-Allow-Origin": "http://localhost:9000"
+    })
     response.end('hello,ida')
 
 }).listen(9001)
